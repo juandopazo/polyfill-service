158e03211291e56353723e39bbb2a7d476f9b80c
Juan Ignacio Dopazo <juan@dopazo.me>
2014-11-03 12:45:02 -0300

    Add correct feature test for Promise polyfill

--- polyfills/Promise/detect.js
+++ polyfills/Promise/detect.js
@@ -1,1 +1,21 @@
-"Promise" in window
+// Some of these methods are missing from
+// Firefox/Chrome experimental implementations
+"Promise" in window &&
+    "resolve" in Promise &&
+    "reject" in Promise &&
+    "race" in Promise &&
+    "all" in Promise &&
+    (function () {
+        var p = Promise.resolve(),
+            resolve;
+
+        new Promise(function (r) {
+            resolve = r;
+        });
+
+        // An older version of the spec had a resolver object
+        // as the arg rather than a function
+        // Also, another version of the spec had a Promise.cast() method and
+        // Promise.resolve always returned a new Promise object
+        return typeof resolve === 'function' && p === Promise.resolve(p);
+    }())
